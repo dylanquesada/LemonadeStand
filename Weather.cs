a9061147ca395336ae9d5b958386f9b95b3372f9
Dylan <dquesada186@gmail.com>
2018-01-03 11:57:27 -0600

    Added empty classes and Weather GenerateTemperature method.

---
+++ Weather.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Weather
+    {
+        //member variables
+        private int temperature;
+        private string atmosphere;
+        private List<string> atmospheres = new List<string> {
+            "Precipiation",
+            "Overcast",
+            "Partly Cloudy",
+            "Clear" };
+        Random rnd = new Random();
+        //constructor
+
+
+        //member methods
+        private int generateTemperature()
+        {
+            temperature = rnd.Next(1, 110);
+            return temperature;
+        }
+
+    }
+}
