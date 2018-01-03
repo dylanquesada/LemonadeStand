e21e9b2c258003d4a2f62a7bb0db8166a28d80c9
Dylan <dquesada186@gmail.com>
2018-01-03 14:00:20 -0600

    Added DisplayStore method and UserInterface class.

--- Store.cs
+++ Store.cs
@@ -8,5 +8,41 @@ namespace LemonadeStand
 {
     class Store
     {
+        //member variables
+        private string option;
+        private UserInterface userInterface = new UserInterface();
+        //constructor
+        public Store()
+        {
+            
+        }
+        //member methods
+        public void DisplayStore()
+        {
+            option = "";
+            while (!(option == "done"))
+            {
+                Console.WriteLine("Welcome to the Store. Enter keyword for purchasing or 'Done' to Continue.");
+                option = userInterface.GetUserInput();
+                switch (option)
+                {
+                    case "done":
+                        option = "done";
+                        break;
+                    case "lemons":
+                        Console.WriteLine("");
+                        break;
+                    case "sugar":
+                        Console.WriteLine("");
+                        break;
+                    case "ice":
+                        Console.WriteLine("");
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, '{0}' is not a valid keyword. Please try again.");
+                        break;
+                }
+            }
+        }
     }
 }
