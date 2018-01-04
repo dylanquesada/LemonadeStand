837eba1b9945839992edfc04408b9b29cd7cd187
Dylan <dquesada186@gmail.com>
2018-01-04 09:03:57 -0600

    Added menu method to Store class and member variables for store.

--- Store.cs
+++ Store.cs
@@ -10,6 +10,10 @@ namespace LemonadeStand
     {
         //member variables
         private string option;
+        private int CostOfCups;
+        private int CostOfLemons;
+        private int CostOfIceCubes;
+        private int CostOfSugarUnits;
         private UserInterface userInterface = new UserInterface();
         //constructor
         public Store()
@@ -30,25 +34,35 @@ namespace LemonadeStand
                         option = "done";
                         break;
                     case "cups":
-                        player.BuyCups();
+                        userInterface.DisplayStoreRequest(option);
+                        player.BuyStuff(CostOfCups , CollectQuantityRequested());
                         break;
                     case "lemons":
-                        Console.WriteLine("");
-                        player.BuyLemons();
+                        userInterface.DisplayStoreRequest(option);
+                        player.BuyStuff(CostOfLemons, CollectQuantityRequested());
                         break;
-                    case "sugar":
-                        Console.WriteLine("");
-                        Player.BuySugar();
+                    case "sugar units":
+                        userInterface.DisplayStoreRequest(option);
+                        player.BuyStuff(CostOfSugarUnits, CollectQuantityRequested());
                         break;
-                    case "ice":
-                        Player.BuyIce();
-                        Console.WriteLine("");
+                    case "ice cubes":
+                        userInterface.DisplayStoreRequest(option);
+                        player.BuyStuff(CostOfIceCubes, CollectQuantityRequested());
                         break;
                     default:
-                        Console.WriteLine("Sorry, '{0}' is not a valid keyword. Please try again.");
+                        Console.WriteLine("Sorry, '{0}' is not a valid keyword. Please try again.", option);
+                        DisplayStore(player);
                         break;
                 }
             }
+        }
+        private void DisplayBuyOptions(string type)
+        {
+            userInterface.DisplayStoreRequest(type);
+        }
+        private int CollectQuantityRequested()
+        {
+            return userInterface.GetUserIntInput();
         }
        
     }
