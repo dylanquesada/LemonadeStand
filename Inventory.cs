f5c297fe428e7579c09682a9a778e584c7a7bf0f
Dylan <dquesada186@gmail.com>
2018-01-04 08:31:18 -0600

    Added member variables and accessors for member variables in Inventory class.

--- Inventory.cs
+++ Inventory.cs
@@ -9,9 +9,81 @@ namespace LemonadeStand
     class Inventory
     {
         //member variables
+        private int numberOfCups;
+        private int numberOfLemons;
+        private int numberOfIcecubes;
+        private int numberOfSugarUnits;
+        public int NumberOfCups
+        {
+            get
+            {
+                return numberOfCups;
+            }
+            set
+            {
+                numberOfCups = value;
+            }
+        }
+        public int NumberOfLemons
+        {
+            get
+            {
+                return numberOfLemons;
+            }
+            set
+            {
+                numberOfLemons = value;
+            }
+        }
+        public int NumberOfIcecubes
+        {
+            get
+            {
+                return numberOfIcecubes;
+            }
+            set
+            {
+                numberOfIcecubes = value;
+            }
+        }
+        public int NumberOfSugarUnits
+        {
+            get
+            {
+                return numberOfSugarUnits;
+            }
+            set
+            {
+                numberOfSugarUnits = value;
+            }
+        }
 
         //constructor
+        public Inventory()
+        {
+
+        }
 
         //member methods
+        public void AddToInventory(int quantity, string type)
+        {
+            switch (type)
+            {
+                case "cups":
+
+                    break;
+                case "lemons":
+
+                    break;
+                case "ice":
+
+                    break;
+                case "sugar":
+
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
